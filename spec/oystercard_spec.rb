cd59c1b23a616a2b0e4ebdf66b8fc1f7d6e8e2cb
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-09-10 14:43:04 +0100

    initiated with empty balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
-describe Oysercard do
-  
+require 'oystercard'
+
+describe Oystercard do
+      it { is_expected.to respond_to(:balance) }
+      describe '#initialize' do
+        it 'Is initially empty' do
+          expect(subject.balance).to eq(0)
+        end
+      end
+
 end
---
+++ userstories.md
@@ -0,0 +1,8 @@
+In order to use public transport
+As a customer
+I want money on my card
+
+in irb
+require './lib/oystercard.rb'
+test = Oystercard.new
+test.balance == 0
